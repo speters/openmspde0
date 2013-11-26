565f6132e8cd2297897b4a7b2ab3c16185f3d842
Soenke J. Peters <peters__git@opcenter.de>
2013-11-26 12:15:10 +0100

    First successful compile using new Verilog inclusion startegy

--- openMSP430.v
+++ openMSP430.v
@@ -2,8 +2,8 @@
 // Configuration
 // Keep in sync with config.vhd!
 //=============================================================================
-`include "openMSP430_defines.v"
 `define OMSP_NO_INCLUDE
+`include "openMSP430_defines.v"
 
 //=============================================================================
 // FPGA Specific modules
@@ -17,27 +17,27 @@
 // openMSP430
 //=============================================================================
 
-`include "external/opencores.org_-_openmsp430/core/rtl/verilog/openmsp430/openMSP430.v"
-`include "external/opencores.org_-_openmsp430/core/rtl/verilog/openmsp430/omsp_frontend.v"
-`include "external/opencores.org_-_openmsp430/core/rtl/verilog/openmsp430/omsp_execution_unit.v"
-`include "external/opencores.org_-_openmsp430/core/rtl/verilog/openmsp430/omsp_register_file.v"
-`include "external/opencores.org_-_openmsp430/core/rtl/verilog/openmsp430/omsp_alu.v"
-`include "external/opencores.org_-_openmsp430/core/rtl/verilog/openmsp430/omsp_sfr.v"
-`include "external/opencores.org_-_openmsp430/core/rtl/verilog/openmsp430/omsp_mem_backbone.v"
-`include "external/opencores.org_-_openmsp430/core/rtl/verilog/openmsp430/omsp_clock_module.v"
-`include "external/opencores.org_-_openmsp430/core/rtl/verilog/openmsp430/omsp_dbg.v"
-`include "external/opencores.org_-_openmsp430/core/rtl/verilog/openmsp430/omsp_dbg_hwbrk.v"
-`include "external/opencores.org_-_openmsp430/core/rtl/verilog/openmsp430/omsp_dbg_uart.v"
-`include "external/opencores.org_-_openmsp430/core/rtl/verilog/openmsp430/omsp_dbg_i2c.v"
-`include "external/opencores.org_-_openmsp430/core/rtl/verilog/openmsp430/omsp_watchdog.v"
-`include "external/opencores.org_-_openmsp430/core/rtl/verilog/openmsp430/omsp_multiplier.v"
-`include "external/opencores.org_-_openmsp430/core/rtl/verilog/openmsp430/omsp_sync_reset.v"
-`include "external/opencores.org_-_openmsp430/core/rtl/verilog/openmsp430/omsp_sync_cell.v"
-`include "external/opencores.org_-_openmsp430/core/rtl/verilog/openmsp430/omsp_scan_mux.v"
-`include "external/opencores.org_-_openmsp430/core/rtl/verilog/openmsp430/omsp_and_gate.v"
-`include "external/opencores.org_-_openmsp430/core/rtl/verilog/openmsp430/omsp_wakeup_cell.v"
-`include "external/opencores.org_-_openmsp430/core/rtl/verilog/openmsp430/omsp_clock_gate.v"
-`include "external/opencores.org_-_openmsp430/core/rtl/verilog/openmsp430/omsp_clock_mux.v"
-`include "external/opencores.org_-_openmsp430/core/rtl/verilog/openmsp430/periph/omsp_gpio.v"
-`include "external/opencores.org_-_openmsp430/core/rtl/verilog/openmsp430/periph/omsp_timerA.v"
+`include "external/opencores.org_-_openmsp430/core/rtl/verilog/openMSP430.v"
+`include "external/opencores.org_-_openmsp430/core/rtl/verilog/omsp_frontend.v"
+`include "external/opencores.org_-_openmsp430/core/rtl/verilog/omsp_execution_unit.v"
+`include "external/opencores.org_-_openmsp430/core/rtl/verilog/omsp_register_file.v"
+`include "external/opencores.org_-_openmsp430/core/rtl/verilog/omsp_alu.v"
+`include "external/opencores.org_-_openmsp430/core/rtl/verilog/omsp_sfr.v"
+`include "external/opencores.org_-_openmsp430/core/rtl/verilog/omsp_mem_backbone.v"
+`include "external/opencores.org_-_openmsp430/core/rtl/verilog/omsp_clock_module.v"
+`include "external/opencores.org_-_openmsp430/core/rtl/verilog/omsp_dbg.v"
+`include "external/opencores.org_-_openmsp430/core/rtl/verilog/omsp_dbg_hwbrk.v"
+`include "external/opencores.org_-_openmsp430/core/rtl/verilog/omsp_dbg_uart.v"
+`include "external/opencores.org_-_openmsp430/core/rtl/verilog/omsp_dbg_i2c.v"
+`include "external/opencores.org_-_openmsp430/core/rtl/verilog/omsp_watchdog.v"
+`include "external/opencores.org_-_openmsp430/core/rtl/verilog/omsp_multiplier.v"
+`include "external/opencores.org_-_openmsp430/core/rtl/verilog/omsp_sync_reset.v"
+`include "external/opencores.org_-_openmsp430/core/rtl/verilog/omsp_sync_cell.v"
+`include "external/opencores.org_-_openmsp430/core/rtl/verilog/omsp_scan_mux.v"
+`include "external/opencores.org_-_openmsp430/core/rtl/verilog/omsp_and_gate.v"
+`include "external/opencores.org_-_openmsp430/core/rtl/verilog/omsp_wakeup_cell.v"
+`include "external/opencores.org_-_openmsp430/core/rtl/verilog/omsp_clock_gate.v"
+`include "external/opencores.org_-_openmsp430/core/rtl/verilog/omsp_clock_mux.v"
+`include "external/opencores.org_-_openmsp430/core/rtl/verilog/periph/omsp_gpio.v"
+`include "external/opencores.org_-_openmsp430/core/rtl/verilog/periph/omsp_timerA.v"
 
