17ecbe4924f073ad137ea3649d8844cefd3370f6
Soenke J. Peters <peters__git@opcenter.de>
2013-12-05 11:43:52 +0100

    Improvements to simulation

--- tb_template_periph_16b.vhd
+++ tb_template_periph_16b.vhd
@@ -2,6 +2,7 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.std_logic_misc.all;
 use ieee.numeric_std.all;
+use std.textio.all;
 
 use work.config.all;
 
@@ -35,6 +36,14 @@ signal puc_rst : std_logic := '0';
 -- constant clk_period : time := (10**9/DCO_FREQ)*1ns;
 constant clk_period : time := 20 ns;
 
+signal sim_finished : std_logic := '0';
+signal sim_info : string(1 to 64);
+
+function string_pad(instring: string; strlen: natural) return string is
+begin
+	return instring & (instring'length to strlen-1 => ' ');
+end function;
+
 begin
 	uut: template_periph_16b
 		generic map (DEC_WD => 3, BASE_ADDR => 400)
@@ -50,27 +59,58 @@ begin
 
 	clk_process: process
 	begin
-		mclk <= '0';
-		wait for clk_period/2;
 		mclk <= '1';
 		wait for clk_period/2;
+		mclk <= '0';
+		wait for clk_period/2;
+
+		if (sim_finished = '1') then
+			wait;
+		end if;
 	end process;
 	
 	stim_process: process
 	begin
+		sim_info <= string_pad("reset", sim_info'length);
 		puc_rst <= '1';
 		wait for clk_period;
+		
+		sim_info <= string_pad("write to register 0", sim_info'length);
 		puc_rst <= '0';
-		wait for clk_period*2;
 		per_en <= '1';
 		per_addr <= std_logic_vector(to_unsigned(16#190#, PER_MSB+1));
 		per_we <= "11";
 		per_din <= x"AAAA";
 		wait for clk_period*2;
+		
+		sim_info <= string_pad("try to write to register 0 with we=0 (must fail)", sim_info'length);
 		per_we <= "00";
 		per_din <= x"F0F0";
-		wait for clk_period*2;
+		wait for clk_period;
+		
+		sim_info <= string_pad("write to register 1 (offset 2)", sim_info'length);
+		per_we <= "11";
+		per_addr <= std_logic_vector(to_unsigned(16#192#, PER_MSB+1));
+		per_din <= x"FFF0";
+		wait for clk_period;
+		
+		sim_info <= string_pad("read back register 0", sim_info'length);
+		per_addr <= std_logic_vector(to_unsigned(16#190#, PER_MSB+1));
+		per_we <= "00";
+		per_din <= x"00FF";
+		wait for clk_period;
+		
+		sim_info <= string_pad("disable peripheral", sim_info'length);
 		per_en <= '0';
+		wait for clk_period;
+		
+		sim_info <= string_pad("enable peripheral", sim_info'length);
+		per_en <= '1';
+		wait for clk_period;
+		
+		sim_info <= string_pad("reset", sim_info'length);
+		puc_rst <= '1';
+		sim_finished <= '1';
 		wait;
 	end process;
 	
--- template_periph_16b.vhd
+++ template_periph_16b.vhd
@@ -27,7 +27,7 @@ end entity;
 
 architecture rtl of template_periph_16b is
 	constant BASE_ADDR_SLV : std_logic_vector( PER_MSB downto 0 ) := std_logic_vector(to_unsigned(BASE_ADDR, PER_MSB+1));
-	signal local_addr: unsigned(DEC_WD -2 downto 0) := unsigned(per_addr(DEC_WD -1 downto 1)); -- aligned to 16bit data width
+	signal local_addr: unsigned(DEC_WD -2 downto 0);
 	
 	signal reg_sel : std_logic;
 
@@ -40,17 +40,23 @@ architecture rtl of template_periph_16b is
 	type t_Register is array(0 to (DEC_SZ - 1) ) of unsigned(per_din'range);
 	signal Cntrl : t_Register;
 	signal Cntrl_rd : t_Register;
+	signal Cntrl_wr : t_Register;
 
 begin
 	-- Test if this peripheral is addressed
 	reg_sel <= per_en when ( per_addr(PER_MSB downto DEC_WD ) = BASE_ADDR_SLV(PER_MSB downto DEC_WD) )
 				else '0';
+
+	local_addr <= unsigned(per_addr(DEC_WD -1 downto 1)); -- aligned to 16bit data width
+
 	
 	g_reg1: for i in 0 to (DEC_SZ -1 ) generate
 		-- Address decoder
 		reg_dec(i) <= '1' when (to_integer(local_addr) = i) else '0';
 		reg_wr(i) <= (reg_sel and (per_we(0) or per_we(1))) when (to_integer(local_addr) = i) else '0';
 		reg_rd(i) <= reg_sel when (to_integer(local_addr) = i) else '0';
+
+		Cntrl_wr(i) <= unsigned(per_din) when (reg_wr(i) = '1') else Cntrl(i);
 	
 		p_reg1: process begin
 			wait until (rising_edge(mclk));
@@ -58,9 +64,7 @@ begin
 			if ( puc_rst = '1' ) then -- synchronous reset
 				Cntrl(i) <= (Cntrl(i)'range => '0') ;
 			else 
-				if ( reg_wr(i) = '1' ) then 
-					Cntrl(i) <= unsigned(per_din);
-				end if;
+				Cntrl(i) <= Cntrl_wr(i);
 			end if;
 		end process;
 		
